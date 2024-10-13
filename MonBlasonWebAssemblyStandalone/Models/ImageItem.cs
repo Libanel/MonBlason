a154a4e60115bec78fc085821f137b319480137e
CYTech Student <louisarmand201@gmail.com>
2024-10-13 23:56:33 +0200

    update final 1

---
+++ MonBlasonWebAssemblyStandalone/Models/ImageItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MonBlasonWebAssemblyStandalone.Models
+{
+    public class ImageItem
+    {
+        public string ImageUrl { get; set; }
+        public string Text { get; set; }
+        public string AltText { get; set; }
+    }
+}
